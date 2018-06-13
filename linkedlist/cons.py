302ff0102efd38e4e35604a8d6c574d1128b5350
JavierCvilla <javiercvilla@gmail.com>
2018-06-13 09:29:27 +0200

    Implement iterator protocol

--- linkedlist/cons.py
+++ linkedlist/cons.py
@@ -22,3 +22,35 @@ class Cons:
 	#    self = self.tail
 	#    count += 1
 	#return count
+
+    # Alternative implementation of iterator
+    # using a generator function
+    def __iter__(self):
+        cons = self
+        while cons is not None:
+            yield cons.item
+            cons = cons.tail
+
+    #def __iter__(self):
+    #    return ConsIterator(self)
+
+
+### Class not needed since we have implemented
+### the iterator protocol using a generator function
+### in the class itself
+#class ConsIterator:
+#    def __init__(self, cons):
+#        self._cons = cons
+#
+#    def __iter__(self):
+#        return self
+#
+#    def __next__(self):
+#        if self._cons is None:
+#            raise StopIteration
+#        item = self._cons.item
+#        self._cons = self._cons.tail
+#        return item
+#
+#    next = __next__
+###
